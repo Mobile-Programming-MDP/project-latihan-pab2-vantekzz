94eef86c634e64b00af7518102453e15e3aea070
vantekzz <ivanarman311@gmail.com>
2025-02-20 20:19:55 +0700

    p

---
+++ daftar_karyawan/android/app/src/main/kotlin/asd/daftar_karyawan/MainActivity.kt
@@ -0,0 +1,5 @@
+package asd.daftar_karyawan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ daftar_karyawan/lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:daftar_karyawan/models/karyawan.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black, secondary: Colors.cyan),
+          useMaterial3: true,
+      ),
+      home: MyhomePage(),
+    ):
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  Future<List<Karyawan>>  readjsonData() async {
+    final String response = await rootBundle.loadString('assets/karyawan.json');
+    final List<dynamic> data = json.decode(response);
+    return data.map((json) = Karyawan.fromJson(json)).toList();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        tittle : const Text("Daftar Karyawan"),
+      ),
+      body: FutureBuilder<List<Karyawan>>(
+        future: _readJsonData{},
+        builder: (context, snapshot)
+        if(snapshot.hasData) {
+          return ListView.builder(itemBuilder: (context, index) {
+            return ListTile(
+              tittle : text(initialData: [snapshot.],)
+            )
+          })
+        }
+      )
+    )
+  }
+}
